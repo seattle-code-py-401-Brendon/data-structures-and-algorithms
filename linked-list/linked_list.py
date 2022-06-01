d9d1c86bcb9f0f4d85df449788536ddd2a4f88e4
Brendon <Brendonhampton1988@gmail.com>
2022-05-31 21:24:07 -0700

    added data structures and algorithms folder

---
+++ linked-list/linked_list.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+
+class Linked_List:
+    def __init__(self, head=None):
+        self.head = head
